3749f29918719f27bb019068d7d8100f15a86e72
Aleksey <aleksey.ukrnets@mail.ru>
2021-04-11 20:35:45 +0300

    Site-1
    Site-1

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0)
+}) //sticky scroll
+const toggleMenu = () => {
+const menuToggle = document.querySelector('.menuToggle');
+const navigation = document.querySelector('.navigation');
+menuToggle.classList.toggle('active')
+navigation.classList.toggle('active')
+}
